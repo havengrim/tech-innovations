84e543180c7f283bdf933c34a7298a4a7242d606
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-10-09 08:14:21 +0800

    overflow

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -10,7 +10,7 @@ import Footer from "@/components/Footer"
 
 const Home = () => {
   return (
-    <div>
+    <div className="overflow-hidden">
         <Navbar />
         <Hero />
         <AboutUs />
